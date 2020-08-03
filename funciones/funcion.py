3ff8773fd8b341e90b8c54f24a13c868572979c8
Albeiro Muriel <j.albeiromuriel@gmail.com>
2020-08-03 01:21:45 -0500

    funciones

---
+++ funciones/funcion.py
@@ -0,0 +1,27 @@
+def mensaje():
+    print('hola mundo')
+
+def retornarNombre(name='No ingreso valor'):
+    print (f'Hola {name} como estas....')
+
+
+def suma(a, b):
+    return  a + b
+
+     
+if __name__ == "__main__":
+    mensaje()
+
+    # funciones con parametros
+    retornarNombre('albeiro')
+    retornarNombre('Liliana')
+
+    print(suma(50,100))
+
+    
+
+
+
+
+
+
